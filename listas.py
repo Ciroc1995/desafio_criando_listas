914f3f3089b74efbf1068267b179f5a359dab4dd
Cicero Cordeiro <cicero.cordeiro1995@gmail.com>
2022-12-19 16:57:55 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,13 @@
+# Desafio 1
+objetos = ['Celular', 'Computador', 'Carro']
+print(objetos)
+
+# Desafio 2
+lista_de_numeros = list(range(10, 132))
+
+# Desafio 3
+print(objetos + lista_de_numeros)
+
+# Desafio 4
+itens = [['Celular', 1100], ['Computador', 5200], ['Carro', 30000]]
+print(itens[1][1])
